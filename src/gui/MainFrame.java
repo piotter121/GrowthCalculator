ac712c102dcc5dec7933df7fcd8b6e0f6ba5a4e3
piotter121 <defcon1994@gmail.com>
2015-11-17 00:03:38 +0100

    Dodanie biblioteki JFreeChart i stworzenie przykładowego wykresu

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,5 +1,11 @@
 package gui;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.DefaultXYDataset;
+
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
@@ -10,28 +16,38 @@ import java.awt.*;
  */
 public class MainFrame extends JFrame {
 
-    private JPanel contentPane = new JPanel();
-    private JPanel chartPanel = new JPanel();
-    private JPanel rightPanel = new JPanel();
+    private JPanel contentPane;
+    private ChartPanel chartPanel;
+    private JPanel rightPanel;
     private JTable resultsTable;
-    private JPanel buttonsPanel = new JPanel(new FlowLayout());
-    private JButton loadButton = new JButton("Wczytaj dane");
-    private JButton saveButton = new JButton("Zapisz dane");
-    private JButton optionsButton = new JButton("Opcje obliczeń");
+    private JPanel buttonsPanel;
+    private JButton loadButton;
+    private JButton saveButton;
+    private JButton optionsButton;
 
     public MainFrame () {
         super("Kalkulator rozwoju dziecka");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000,700);
+        setSize(1030,700);
+
+        contentPane = new JPanel();
+        createChartPanel();
+        rightPanel = new JPanel();
+        buttonsPanel = new JPanel(new FlowLayout());
+        loadButton = new JButton("Wczytaj dane");
+        saveButton = new JButton("Zapisz dane");
+        optionsButton = new JButton("Opcje obliczeń");
 
         // Ustawienie głównego kontenera
         setContentPane(contentPane);
         contentPane.setLayout(new FlowLayout());
 
         rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        //rightPanel.setPreferredSize(new Dimension(300,700));
 
         createTable();
         JScrollPane scrollpane = new JScrollPane(resultsTable);
+        scrollpane.setBackground(Color.blue);
 
         configureButtons();
 
@@ -41,9 +57,21 @@ public class MainFrame extends JFrame {
         contentPane.add(rightPanel);
     }
 
+    private void createChartPanel() {
+        DefaultXYDataset ds = new DefaultXYDataset();
+        double[][] data = { {0.1, 0.2, 0.3}, {1, 2, 3} };
+        ds.addSeries("series1", data);
+        JFreeChart chart = ChartFactory.createXYLineChart("Test Chart",
+                "x", "y", ds, PlotOrientation.VERTICAL, true, true,
+                false);
+        chartPanel = new ChartPanel(chart);
+        chartPanel.setBackground(Color.cyan);
+    }
+
     private void configureButtons() {
         // TO DO dodać action listenery
 
+        buttonsPanel.setBackground(Color.yellow);
         buttonsPanel.add(loadButton);
         buttonsPanel.add(saveButton);
         buttonsPanel.add(optionsButton);
@@ -51,9 +79,25 @@ public class MainFrame extends JFrame {
 
     private void createTable() {
         TableModel dataModel = new AbstractTableModel() {
-            public int getColumnCount() { return 2; }
-            public int getRowCount() { return 10;}
-            public Object getValueAt(int row, int col) { return 0; }
+            @Override
+            public String getColumnName(int column) {
+                switch (column) {
+                    case 0:
+                        return "Wiek";
+                    case 1:
+                        return "Wzrost";
+                }
+                return "";
+            }
+            public int getColumnCount() {
+                return 2;
+            }
+            public int getRowCount() {
+                return 10;
+            }
+            public Object getValueAt(int row, int col) {
+                return 0;
+            }
         };
         resultsTable = new JTable(dataModel);
     }
